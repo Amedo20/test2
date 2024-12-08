42865898dcc0722c5c32c260ce0660f36eacdbf0
Akhmad <example@gmail.com>
2024-12-08 16:57:44 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let date = '28.02.1999'; //string
+const pi = 3;
+const a = 14;
+a = 2;
+
+let name = prompt('name');
+let address = prompt('address');
+let phone = prompt('phone');
+
+console.log(`Студент ${name}, Адрес: ${address}, номер телефона: ${phone}`);
+
+let roubleToDollar = 100;
+let roublesAvailable = 2000;
+let result = roublesAvailable / roubleToDollar;
+console.log(result)
